a2e99587c7f3ef7090bd43762d021ffcb570bba5
Yongze Xu <yongzxu@YONGZXU-M-H04G.CISCO.COM>
2014-06-28 08:43:45 +0800

    add xcode ios for exosip

---
+++ exosip-4.1.0/platform/ios/exosip/exosip/exosip.h
@@ -0,0 +1,13 @@
+//
+//  exosip.h
+//  exosip
+//
+//  Created by Yongze Xu on 6/28/14.
+//  Copyright (c) 2014 uskee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface exosip : NSObject
+
+@end
