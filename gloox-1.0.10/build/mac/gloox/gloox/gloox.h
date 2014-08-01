ddb7cb88798c607c1ec733358d79c984d2bd3c0a
Yongze Xu <test@test>
2014-08-01 11:57:08 +0800

    add mac proj for gloox

---
+++ gloox-1.0.10/build/mac/gloox/gloox/gloox.h
@@ -0,0 +1,13 @@
+//
+//  gloox.h
+//  gloox
+//
+//  Created by Yongze Xu on 8/1/14.
+//  Copyright (c) 2014 uskee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface gloox : NSObject
+
+@end
---
+++ gloox-1.0.10/build/mac/gloox/gloox/gloox.m
@@ -0,0 +1,13 @@
+//
+//  gloox.m
+//  gloox
+//
+//  Created by Yongze Xu on 8/1/14.
+//  Copyright (c) 2014 uskee. All rights reserved.
+//
+
+#import "gloox.h"
+
+@implementation gloox
+
+@end
